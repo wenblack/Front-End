b0d5b66e1879625a2cc6b72272ebb8669bf3d63e
wenblack <wenblack012@gmail.com>
2016-10-25 00:34:24 -0200

    Criaação e estilização da página produto utilizando o conceito Mobile First

--- root/produto.php
+++ root/produto.php
@@ -8,37 +8,91 @@
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="css/produto.css">
 		<link rel="stylesheet" href="css/reset.css">
-		<link rel="stylesheet" href="css/estilo.css">	
+		<link rel="stylesheet" href="css/estilo.css">
+		
 	</head>
 
-	<body>		
-		<div class="produto">
-			<h1>Fuzzy Cardigan</h1>
-			<p>por apenas R$ 129,00</p>
-			<form>
-				<fieldset class="cores">
-					<legend>Escolha a cor:</legend>
-					<input type="radio" name="cor" value="verde" id="verde" checked>
-					<label for="verde">
-						<img src="img/produtos/foto2-verde.png" alt="verde">
-					</label>
+	<body>
+		<div class="produto-back">
+			<div class="container">
+				<div class="produto">
+					<h1>Fuzzy Cardigan</h1>
+					<p>por apenas R$ 129,00</p>
 					
-					<input type="radio" name="cor" value="rosa" id="rosa">
+					<form>
+						<fieldset class="cores">
+							<legend>Escolha a cor:</legend>
+							<input type="radio" name="cor" value="verde" id="verde" checked>
+							<label for="verde">
+								<img src="img/produtos/foto2-verde.png" alt="verde">
+							</label>
 					
-					<label for="rosa">
-						<img src="img/produtos/foto2-rosa.png" alt="rosa">
-					</label>
+							<input type="radio" name="cor" value="rosa" id="rosa">
 					
-					<input type="radio" name="cor" value="azul" id="azul">
-					<label for="azul">
-						<img src="img/produtos/foto2-azul.png" alt="azul">
-					</label>
-
-				</fieldset>
-				<input type="submit" class="comprar" value="Comprar">
-			</form>
-		</div>
+							<label for="rosa">
+								<img src="img/produtos/foto2-rosa.png" alt="rosa">
+							</label>
+					
+							<input type="radio" name="cor" value="azul" id="azul">
+							<label for="azul">
+								<img src="img/produtos/foto2-azul.png" alt="azul">
+							</label>
+							
+							<input type="submit" class="comprar" value="Comprar">
+							
+						</fieldset>
+						
+						<fieldset class="tamanhos">
+							<legend>Escolha o tamanho:</legend>
+							<input type="range" min="36" max="46" value="42" step="2"name="tamanho" id="tamanho">
+						</fieldset>
+						
+					</form>
+					
+				</div>
+			
+				<div class="detalhes">
+					<h2>Detalhes do produto</h2>
+						<p>
+							Esse é o melhor casaco de Cardigã que você já viu. Excelente material italiano com estampa desenhada pelos artesãos da comunidade de Krotor nas ilhas gregas. Compre já e receba hoje mesmo pela nossa entrega a jato.
+						</p>
+						
+					<table>
+						<thead>
+							<tr>
+								<th>Característica</th>
+								<th>Detalhe</th>
+							</tr>
+						</thead>
+						
+						<tbody>
+							<tr>
+								<td>Modelo</td>
+								<td>Cardigã 7845</td>
+							</tr>
+							
+							<tr>
+								<td>Material</td>
+								<td>Algodão e poliester</td>
+							</tr>
 		
+							<tr>
+								<td>Cores</td>
+								<td>Azul, Rosa e Verde</td>
+							</tr>
+						
+							<tr>
+								<td>Lavagem</td>
+								<td>Lavar a mão</td>
+							</tr>
+						
+						</tbody>
+						
+					</table>
+				</div>
+			
+			</div>	
+		</div>
 		
 		<?php include("rodape.php"); ?>
 	</body>
